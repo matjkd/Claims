d1c3754e88e5ff7c0454d51f566531e71e0ac3f1
Mat Sadler <mat@redstudio.co.uk>
2012-09-13 14:56:46 +0000

    updated advice menu

--- application/views/global/claims/advice_menu.php
+++ application/views/global/claims/advice_menu.php
@@ -1,1 +1,27 @@
-advice menu
+
+<div class="menuItem1">
+	<a href="<?=base_url()?>welcome/home/how_much_can_i_claim">
+	 How Much Can I Claim?
+	 </a>
+</div>
+
+<div class="menuItem2">
+	<a href="<?=base_url()?>welcome/home/what_is_no_win_no_fee">
+	What is No Win No Fee?
+	</a>
+</div>
+<div class="menuItem3">
+	<a href="<?=base_url()?>welcome/home/how_does_a_claim_work">
+	How Does a Claim Work?
+	</a>
+</div>
+<div class="menuItem4">
+	<a href="<?=base_url()?>welcome/home/faqs">
+	FAQS
+	</a>
+</div>
+<div class="menuItem5">
+	<a href="<?=base_url()?>welcome/home/need_some_more_advice">
+	Need Some More Advice?
+	</a>
+</div>
